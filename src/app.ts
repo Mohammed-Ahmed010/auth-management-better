3b6a9e0a5eacfa7f5b3f9a6548a55687d4f72e20
Mohammed-Ahmed010 <mohammedahme1212@gmail.com>
2025-07-19 04:48:23 +0530

    added-sessions

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,21 @@ import dotenv from "dotenv";
 import { requireAuth } from "./middlewares/auth";
 import authRouter from "./routes/auth";
 import morgan from "morgan";
+import session from "express-session";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(
+	session({
+		// store,
+		secret: process.env.JWT_SECRET!,
+		resave: false,
+		saveUninitialized: false,
+		cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+	})
+);
 app.use("/", authRouter);
 app.get("/", (req: Request, res: Response) => {
 	return res.send("home");
